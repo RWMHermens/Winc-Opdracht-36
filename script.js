75c7cb892b3271c266a41cebdfb990cbebf06185
Rob Hermens <robhermens@gmail.com>
2021-05-28 17:08:24 +0200

    Basic Arrow Functions

---
+++ script.js
@@ -0,0 +1,43 @@
+// Deel A
+
+// zet de functie om naar een arrow functie
+// const ikRockArrowFunctions = function () {
+//     console.log("Joe, ik rock de arrow functions!");
+// };
+
+// ikRockArrowFunctions()
+
+// arrow functie
+let ikRockArrowFunctions = () => console.log("Joe, ik rock de arrow functions!");
+ikRockArrowFunctions();
+
+
+// Deel B
+
+// const fivePlusSeven = function () {
+//     return 5 + 7;
+// };
+
+// fivePlusSeven();
+
+// arrow functie op 1 regel
+const fivePlusSeven = () => 5 + 7;
+fivePlusSeven();
+
+
+// Deel C
+
+let myFunction = (a, b) => a + b;
+myFunction();
+
+
+// Deel D
+
+let addFive = a => a + 5;
+addFive();
+
+
+// Deel E
+
+const createObject = () => ({ greeting: 'Hoi' });
+createObject();
